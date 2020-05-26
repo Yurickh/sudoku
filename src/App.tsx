6dce762d7f3c698fb41c5b209e2800f842e2f65e
Yurick <yurick.hauschild@gmail.com>
2020-05-26 20:42:46 +0200

    ✨ Add basic numeric inputting capabilities

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,85 @@
-import React from 'react'
-import logo from './logo.svg'
+import React, { ChangeEvent } from 'react'
+import produce from 'immer'
 import './App.css'
 
+type Cell = number | ''
+type Row = Cell[]
+type Box = Row[]
+
+const initialState: Box[] = [...Array(9)].map(() =>
+  [...Array(3)].map(() => [...Array(3)].fill('')),
+)
+
+interface Action {
+  value: Cell
+  box: number
+  row: number
+  cell: number
+}
+
+const boxesReducer = produce((draft: Box[], action: Action) => {
+  draft[action.box][action.row][action.cell] = action.value
+})
+
+const parseDifference = (previousValue: Cell, newValue: number) => {
+  if (newValue === 0) return ''
+  if (previousValue === '') return newValue
+
+  const parsedValue = Number(
+    newValue.toString().replace(previousValue.toString(), ''),
+  )
+
+  if (parsedValue > 0 && parsedValue < 10) {
+    return parsedValue
+  } else {
+    return previousValue
+  }
+}
+
 function App() {
+  const [boxes, dispatch] = React.useReducer(boxesReducer, initialState)
+
+  const handleChange = (box: number, row: number, cell: number) => (
+    event: ChangeEvent<HTMLInputElement>,
+  ) => {
+    const previousValue = boxes[box][row][cell]
+    const value = parseDifference(
+      previousValue,
+      Number(event.currentTarget.value),
+    )
+
+    dispatch({
+      box,
+      row,
+      cell,
+      value,
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App App-header">
+      <h1>Sudoku</h1>
+
+      <div className="game">
+        {boxes.map((box, boxIndex) => (
+          <table className="box" key={boxIndex}>
+            <tbody>
+              {box.map((row, rowIndex) => (
+                <tr className="row" key={rowIndex}>
+                  {row.map((cell, cellIndex) => (
+                    <td className="cell" key={cellIndex}>
+                      <input
+                        value={cell}
+                        onChange={handleChange(boxIndex, rowIndex, cellIndex)}
+                      />
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ))}
+      </div>
     </div>
   )
 }
